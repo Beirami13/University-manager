d801c92618fd8aff53fa76c5cab7197b3705fedd
Beirami13 <Ahoora.beirami@gmail.com>
2026-02-12 21:05:04 -0800

    University

--- University/StudentManager/Student.py
+++ University/StudentManager/Student.py
@@ -4,6 +4,7 @@ class Student(User):
     student = []
     stuCount = 0
 
+
     def __init__(self, name, family, id, password):
         super().__init__(name, family, id, password)
         Student.stuCount += 1
